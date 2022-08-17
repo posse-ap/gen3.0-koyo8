a4e3a9800ac540628e93d69795830b4d178f0578
koyo8 <iida_koyo@yahoo.co.jp>
2022-08-18 00:17:52 +0900

    first

--- QuizPage/quiz.js
+++ QuizPage/quiz.js
@@ -1,32 +1,68 @@
 'use strict';
 
 {
-  const CorrectAnswers = [
+  // const CorrectAnswers = [
+  //   {
+  //     index: 1,
+  //     value: '約79万人',
+  //   },
+  //   {
+  //     index: 2,
+  //     value: 'X-TECH',
+  //   },
+  //   {
+  //     index: 0,
+  //     value: 'Internet of Things',
+  //   },
+  //   {
+  //     index: 0,
+  //     value: 'Society 5.0'
+  //   },
+  //   {
+  //     index: 0,
+  //     value: 'Web3.0'
+  //   },
+  //   {
+  //     index: 1,
+  //     value: '約5倍',
+  //   }
+  // ];
+
+  const AllQuestion{
     {
-      index: 1,
-      value: '約79万人',
+      question: '日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？',
+      answers: ['約28万人', '約79万人', '約183万人'],
+      correctNumber: 1,
+      evidence: '経済産業省 2019年3月 － IT 人材需給に関する調査'
     },
     {
-      index: 2,
-      value: 'X-TECH',
+      question: '既存業界のビジネスと、先進的なテクノロジーを結びつけて生まれた、新しいビジネスのことをなんと言うでしょう？',
+      answers: ['INTECH', 'BIZZTECH', 'X-TECH'],
+      correctNumber: 2,
     },
     {
-      index: 0,
-      value: 'Internet of Things',
+      question: 'IoTとは何の略でしょう？',
+      answers: ['Internet of Things', 'Integrate into Technology', 'Information on Tool'],
+      correctNumber: 1,
     },
     {
-      index: 0,
-      value: 'Society 5.0'
+      question: 'イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？',
+      answers: ['Society 5.0', 'CyPhy', 'SDGs'],
+      correctNumber: 0,
+      evidence: 'Society5.0 - 科学技術政策 - 内閣府'
     },
     {
-      index: 0,
-      value: 'Web3.0'
+      question: 'イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？',
+      answers: ['Web3.0', 'NFT', 'メタバース'],
+      correctNumber: 0,
     },
     {
-      index: 1,
-      value: '約5倍',
+      question: '先進テクノロジー活用企業と出遅れた企業の収益性の差はどれくらいあると言われているでしょうか？',
+      answers: ['約2倍', '約5倍', '約11倍'],
+      correctNumber: 1,
+      evidence: 'Accenture Technology Vision 2021'
     }
-  ];
+  }
 
   const allQuiz = document.querySelectorAll('.js-quiz');
 
