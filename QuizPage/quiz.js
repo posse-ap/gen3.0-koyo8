d5c09b0fb86473ebdc95be2e9548908e0f647a27
koyo8 <iida_koyo@yahoo.co.jp>
2022-10-06 00:22:47 +0900

    クイズimg解決

--- QuizPage/quiz.js
+++ QuizPage/quiz.js
@@ -30,33 +30,39 @@
 
   const AllQuestion = [
     {
+      id: 1,
       question: '日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？',
       answers: ['約28万人', '約79万人', '約183万人'],
       correctNumber: 1,
       evidence: '経済産業省 2019年3月 － IT 人材需給に関する調査',
     },
     {
+      id: 2,
       question: '既存業界のビジネスと、先進的なテクノロジーを結びつけて生まれた、新しいビジネスのことをなんと言うでしょう？',
       answers: ['INTECH', 'BIZZTECH', 'X-TECH'],
       correctNumber: 2,
     },
     {
+      id: 3,
       question: 'IoTとは何の略でしょう？',
       answers: ['Internet of Things', 'Integrate into Technology', 'Information on Tool'],
       correctNumber: 0,
     },
     {
+      id: 4,
       question: 'イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？',
       answers: ['Society 5.0', 'CyPhy', 'SDGs'],
       correctNumber: 0,
       evidence: 'Society5.0 - 科学技術政策 - 内閣府'
     },
     {
+      id: 5,
       question: 'イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？',
       answers: ['Web3.0', 'NFT', 'メタバース'],
       correctNumber: 0,
     },
     {
+      id: 6,
       question: '先進テクノロジー活用企業と出遅れた企業の収益性の差はどれくらいあると言われているでしょうか？',
       answers: ['約2倍', '約5倍', '約11倍'],
       correctNumber: 1,
@@ -79,7 +85,7 @@
   <div class="QProblem">
     <div class="QProblemTitle">Q${questionNumber + 1}</div>
     <p class="QProblemBody">${quizItem.question}</p>
-    <img class="QProblemimg" src="../assets-ph1-website-main/img/quiz/img-quiz0${questionNumber + 1}.png" alt="クイズimg">
+    <img class="QProblemimg" src="../assets-ph1-website-main/img/quiz/img-quiz0${quizItem.id}.png" alt="クイズimg">
   </div>
   <div class="QAnswer">
     <div class="QAnswerTitle">A</div>
