35e2ca672c1cc4679636a6b938101fd66b12402e
koyo8 <iida_koyo@yahoo.co.jp>
2022-10-28 16:12:28 +0900

    完了画面つくり中

--- ph1-webapp/top-page/script.js
+++ ph1-webapp/top-page/script.js
@@ -2,10 +2,12 @@
 
 const openButton = document.querySelector('.header-button');
 const closeButton = document.querySelector('.modal-close-button');
-const inputButton = document.querySelector('.modal-button')
+const inputButton = document.querySelector('.modal-button');
 const body = document.querySelector('#body');
 const modal = document.querySelector('.modal');
-const modalBody = document.querySelector('.modal-body')
+const modalBody = document.querySelector('.modal-body');
+const modalLoad = document.querySelector('.loader');
+const modalComplete = document.querySelector('.modal-complete');
 
 openButton.addEventListener('click' , () => {
   body.classList.add('active');
@@ -20,8 +22,16 @@ closeButton.addEventListener('click' , () => {
   if(modalBody.classList.contains('modal-off')){
     modalBody.classList.remove('modal-off');
   }
+  if(modalComplete.classList.contains('modal-complete-on')){
+    modalComplete.classList.remove('modal-complete-on');
+  }
 })
 
 inputButton.addEventListener('click' , () => {
   modalBody.classList.add('modal-off');
+  modalLoad.classList.add('modal-load-on');
+  window.setTimeout(() => {
+    modalLoad.classList.remove('modal-load-on');
+    modalComplete.classList.add('modal-complete-on');
+}, 0); //実際は5000
 })
